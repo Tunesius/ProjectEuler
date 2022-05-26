8006f7349f41b35b304bedecaffe7ca1ebefcfae
Tunesius <85237243+Tunesius@users.noreply.github.com>
2022-05-26 16:40:41 +0200

    Completed Task 7 :D

---
+++ Problems/7.py
@@ -0,0 +1,18 @@
+prime_number_list = [2, 3]
+current_prime_number_count = 2
+current_number = 5
+
+def is_dividable_by_prime(number):
+    for n in prime_number_list:
+        if number % n == 0:
+            return True
+    return False
+
+while current_prime_number_count < 10001:
+
+    if not is_dividable_by_prime(current_number):
+        current_prime_number_count = current_prime_number_count + 1
+        prime_number_list.append(current_number)
+    current_number = current_number + 1
+
+print(prime_number_list[10000])
