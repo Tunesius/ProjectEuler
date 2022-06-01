2452d6bfb82cc52f70c422bf1d06ba489b5e5740
Tunesius <85237243+Tunesius@users.noreply.github.com>
2022-06-01 17:49:11 +0200

    Completed Tasks 18, 24, 67

---
+++ Problems/24.py
@@ -0,0 +1,40 @@
+# A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+#
+# 012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+import math
+
+
+def give_nth_permutation(n_string, n, p_permutation):
+    if len(n_string) == 2:
+        if n == 1:
+            return p_permutation + n_string
+        else:
+            return p_permutation + n_string[::-1]
+    else:
+        permutation = p_permutation
+        max_permutations = math.factorial(len(n_string))
+        index_calculate = max_permutations / len(n_string)
+        needed_index = int(n//index_calculate)
+        new_n = int((n-1) - (needed_index * index_calculate))
+        new_n_string = n_string.replace(n_string[needed_index], "")
+        permutation = permutation + n_string[needed_index]
+        print("n: " + str(n))
+        print("max_permutations: " + str(max_permutations))
+        print("index_calculate: " + str(index_calculate))
+        print("needed_index: " + str(needed_index))
+        print("new_n: " + str(new_n))
+        print("new_n_string: " + str(new_n_string))
+        print("permutation: " + str(permutation))
+
+        print("NEXXT!!!")
+        if new_n == 1:
+            permutation = permutation + new_n_string
+        else:
+            permutation = give_nth_permutation(new_n_string, new_n, permutation)
+        return permutation
+
+
+print(give_nth_permutation("0123456789", 1_000_000, ""))
+
--- Problems/67.py
+++ Problems/67.py
@@ -7,25 +7,10 @@
 #
 # That is, 3 + 7 + 4 + 9 = 23.
 #
-# Find the maximum total from top to bottom of the triangle below:
+# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 #
-# 75
-# 95 64
-# 17 47 82
-# 18 35 87 10
-# 20 04 82 47 65
-# 19 01 23 75 03 34
-# 88 02 77 73 07 63 67
-# 99 65 04 28 06 16 70 92
-# 41 41 26 56 83 40 80 70 33
-# 41 48 72 33 47 32 37 16 94 29
-# 53 71 44 65 25 43 91 52 97 51 14
-# 70 11 33 28 77 73 17 78 39 68 17 57
-# 91 71 52 38 17 14 91 43 58 50 27 29 48
-# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
-# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+# NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
 
-# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 def import_list_from_file(file_path):
     file = open(file_path)
     names = []
@@ -47,7 +32,7 @@ def make_list_int_list(string_list):
     return return_list
 
 
-final_list = import_list_from_file("p018_triangle.txt")
+final_list = import_list_from_file("p067_triangle.txt")
 for i in range(len(final_list)-2, -1, -1):
     for j in range(len(final_list[i])):
         if final_list[i+1][j] > final_list[i+1][j+1]:
